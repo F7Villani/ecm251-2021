6bca990be9d26fe7dd5f607b2602b177ab16fb0f
F7Villani <felipevillani.2000@hotmail.com>
2021-03-22 08:00:38 -0300

    Criação Validador de CPF

---
+++ Aula05 - Teoria/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        String cpf = "52998224725";
+        System.out.println("CPF Valido: " + ValidadorCPF.validarCPF(cpf));
+    }
+    
+}
