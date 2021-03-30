2c9ca6856a5d5ba9fc6f9564f8b50c49e41af904
F7Villani <felipevillani.2000@hotmail.com>
2021-03-30 14:03:08 -0300

    Update BancoContas.java

--- Atividades/Atividade1/src/villani/freitas/felipe/BancoContas.java
+++ Atividades/Atividade1/src/villani/freitas/felipe/BancoContas.java
@@ -27,6 +27,18 @@ public class BancoContas {
         return true;
      }
 
+     public boolean validarConta(String qrcode){
+
+        String[] dados = qrcode.split(";");
+        int idConta = Integer.parseInt(dados[0]);
+        if (this.bancoContas[idConta-1].getUsuario().getNome().equals(dados[1])){
+            return true;
+        }
+        else{
+            return false;
+        }
+     }
+
     @Override
     public String toString() {
         return bancoContas[0].toString();
