81c0554b32a64c7921c663632f3d26aa0db124f6
F7Villani <felipevillani.2000@hotmail.com>
2021-03-30 11:35:02 -0300

    Update Main.java

--- Atividades/Atividade1/src/villani/freitas/felipe/Main.java
+++ Atividades/Atividade1/src/villani/freitas/felipe/Main.java
@@ -3,6 +3,47 @@ package villani.freitas.felipe;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        Transacao transacao = new Transacao();
+        BancoContas bancoContas = new BancoContas(20);
+
+        Usuario u1 = new Usuario("All Might");
+        Conta c1 = new Conta(1,1000.0,u1);
+
+        bancoContas.addConta(c1);
+        System.out.println(bancoContas.toString());
+
+
+
+        /*
+        Usuario u1 = new Usuario("All Might");
+        Usuario u2 = new Usuario("One For All");
+        Usuario u3 = new Usuario("Bakugo");
+
+        Conta c1 = new Conta(1,1000.0,u1);
+        Conta c2 = new Conta(2,250.0,u1);
+        Conta c3 = new Conta(3,3000.0,u1);
+
+        System.out.println("Estado inicial: ");
+        System.out.println(c1.toString());
+        System.out.println(c2.toString());
+        System.out.println(c3.toString());
+
+        String pagamento1 = transacao.gerarQRCode(c1, 250.0);
+
+        transacao.realizarPagamento(c2,c1,pagamento1);
+        transacao.realizarPagamento(c3,c1,pagamento1);
+        transacao.realizarPagamento(c2,c1,pagamento1);
+
+        String pagamento2 = transacao.gerarQRCode(c2, 1000.0);
+
+        transacao.realizarPagamento(c3,c2,pagamento2);
+
+        System.out.println("Estado final: ");
+        System.out.println(c1.toString());
+        System.out.println(c2.toString());
+        System.out.println(c3.toString());
+
+         */
     }
 }
