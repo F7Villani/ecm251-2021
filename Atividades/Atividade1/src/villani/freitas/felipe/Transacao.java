3ea74b3b6542fe96660d0ad90795faacdd821452
F7Villani <felipevillani.2000@hotmail.com>
2021-03-30 11:35:07 -0300

    Update Transacao.java

--- Atividades/Atividade1/src/villani/freitas/felipe/Transacao.java
+++ Atividades/Atividade1/src/villani/freitas/felipe/Transacao.java
@@ -1,8 +1,37 @@
 package villani.freitas.felipe;
 
+import java.util.Random;
+
 public class Transacao {
 
-    public String gerarQRCode(){
+    // Método que gera um número aleatório entre um intervalo
+    private static int getRandomNumberInRange(int min, int max) {
+        Random r = new Random();
+        return r.nextInt((max - min) + 1) + min;
+    }
 
+    // Método que gera o QRCode em formato de String
+    public String gerarQRCode(Conta conta, double valor){
+        return conta.getIdConta() + ";" + conta.getUsuario().getNome() + ";" + valor + ";" +  getRandomNumberInRange(1000,9999);
+    }
+
+    // Método que realiza um pagamento
+    public boolean realizarPagamento(Conta paga, Conta recebe, String qrcode){
+
+        String[] dados = qrcode.split(";");
+        double valor = Double.parseDouble(dados[2]);
+
+        if(paga.getSaldo() >=valor ){
+            paga.sacar(valor);
+            recebe.depositar(valor);
+        }
+        else{
+            return false;
+        }
+        return true;
+    }
+
+    private void getDepositar(Conta recebe, double valor) {
+        recebe.depositar(valor);
     }
 }
