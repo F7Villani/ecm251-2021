7c63e16c30932b502ca8ec543204d7d57520e38a
F7Villani <felipevillani.2000@hotmail.com>
2021-03-30 10:54:19 -0300

    Cração da classe Usuário

---
+++ Atividades/Atividade1/src/villani/freitas/felipe/Usuario.java
@@ -0,0 +1,19 @@
+package villani.freitas.felipe;
+
+public class Usuario {
+    //Atributos
+    private String nome;
+
+    //Construtor
+
+    public Usuario(String nome) {
+        this.nome = nome;
+    }
+
+    //Getters
+    public String getNome() {
+        return nome;
+    }
+
+
+}
