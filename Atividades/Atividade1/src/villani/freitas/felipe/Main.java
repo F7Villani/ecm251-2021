a8c90c2576bf4ba7af6a530f2dba6f3b5ad9a241
F7Villani <felipevillani.2000@hotmail.com>
2021-03-31 18:45:48 -0300

    Update Main.java

--- Atividades/Atividade1/src/villani/freitas/felipe/Main.java
+++ Atividades/Atividade1/src/villani/freitas/felipe/Main.java
@@ -4,50 +4,50 @@ public class Main {
 
     public static void main(String[] args) {
 
+        // Instanciando um objeto transacao e um bancoContas
         Transacao transacao = new Transacao();
         BancoContas bancoContas = new BancoContas(20);
 
-        Usuario u1 = new Usuario("All Might");
-        Conta c1 = new Conta(1,1000.0,u1);
-
-        bancoContas.addConta(c1);
-        String string1 = transacao.gerarQRCode(c1,2);
-        System.out.println(transacao.gerarQRCode(c1,2));
-        System.out.println(bancoContas.validarConta(string1));
-        /*
+        // Cria os usuários
         Usuario u1 = new Usuario("All Might");
         Usuario u2 = new Usuario("One For All");
         Usuario u3 = new Usuario("Bakugo");
 
-        Conta c1 = new Conta(1,1000.0,u1);
-        Conta c2 = new Conta(2,250.0,u1);
-        Conta c3 = new Conta(3,3000.0,u1);
-
+        // Cria as contas e armazena no bancoContas
+        Conta c1 = new Conta(bancoContas.gerarID(), 1000.0,u1);
         bancoContas.addConta(c1);
+        Conta c2 = new Conta(bancoContas.gerarID(),250.0,u2);
         bancoContas.addConta(c2);
+        Conta c3 = new Conta(bancoContas.gerarID(), 3000.0,u3);
         bancoContas.addConta(c3);
 
+        // Printa o estado inicial das contas
         System.out.println("Estado inicial: ");
         System.out.println(c1.toString());
         System.out.println(c2.toString());
         System.out.println(c3.toString());
 
+        // Gera o primeiro QRCode de pagamento
         String pagamento1 = transacao.gerarQRCode(c1, 250.0);
 
-        transacao.realizarPagamento(c2,c1,pagamento1);
-        transacao.realizarPagamento(c3,c1,pagamento1);
-        transacao.realizarPagamento(c2,c1,pagamento1);
+        // Realiza as transações teste
+        transacao.realizarPagamento(c2,c1,pagamento1,bancoContas);
+        transacao.realizarPagamento(c3,c1,pagamento1,bancoContas);
+        transacao.realizarPagamento(c2,c1,pagamento1,bancoContas);
 
+        // Gera o segundo QRCode de pagamento
         String pagamento2 = transacao.gerarQRCode(c2, 1000.0);
 
-        transacao.realizarPagamento(c3,c2,pagamento2);
+        // Realiza transação teste
+        transacao.realizarPagamento(c3,c2,pagamento2,bancoContas);
 
+        // Printa o estado final das contas
         System.out.println("Estado final: ");
         System.out.println(c1.toString());
         System.out.println(c2.toString());
         System.out.println(c3.toString());
 
-         */
+
 
 
     }
