377e433da2b8bd5bfedd237573b4ddf7695730e1
F7Villani <felipevillani.2000@hotmail.com>
2021-04-22 18:41:37 -0300

    Create Aula07

---
+++ Aula 07/src/villani/freitas/felipe/Main.java
@@ -0,0 +1,9 @@
+package villani.freitas.felipe;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    Pessoa pessoa = new Pessoa("Deku","1243");
+	    System.out.println();
+    }
+}
---
+++ Aula 07/src/villani/freitas/felipe/Pessoa.java
@@ -0,0 +1,13 @@
+package villani.freitas.felipe;
+
+public class Pessoa {
+    private String nome;
+    private String cpf;
+
+    public Pessoa(String nome, String cpf){
+        this.cpf = cpf;
+        this.nome = nome;
+    }
+
+
+}
