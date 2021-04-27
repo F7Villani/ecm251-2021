d1f233ddabbf20bed1c4c44a4f99077aafb75bc4
F7Villani <felipevillani.2000@hotmail.com>
2021-04-27 08:23:11 -0300

    Aula07 - Lab

---
+++ Aula07 - Lab/src/villani/freitas/felipe/Pedra.java
@@ -0,0 +1,19 @@
+package villani.freitas.felipe;
+
+public class Pedra extends Jogada{
+
+    @Override
+    public boolean verificarSeGanhei(Jogada jogada) {
+        return jogada instanceof Tesoura;
+    }
+
+    @Override
+    public boolean verificarSePerdi(Jogada jogada) {
+        return jogada instanceof Papel;
+    }
+
+    @Override
+    public String toString() {
+        return "Pedra";
+    }
+}
---
+++ Aula07 - Lab/src/villani/freitas/felipe/Resultado.java
@@ -0,0 +1,5 @@
+package villani.freitas.felipe;
+
+public enum Resultado {
+    PERDEU, EMPATOU, GANHOU;
+}
